c0fa6a877239f08cd263032cb8c13d6fc0d9e3c4
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-10-29 12:48:57 -0300

    update

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-const root = document.querySelector('#root');
-const elementUl = document.createElement('ul');
-const spinner = document.querySelector('.spinner');
+const root = document.querySelector("#root");
+const elementUl = document.createElement("ul");
+const spinner = document.querySelector(".spinner");
 
 root.append(elementUl);
 
-const rootUl = root.querySelector('ul');
+const rootUl = root.querySelector("ul");
 
-const url = 'https://randomuser.me/api/?results=50';
+const url = "https://randomuser.me/api/?results=50";
 
 let jsonString = [];
 
-fetch(url).
-then( resp => resp.json()).
-then( data => {
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => {
     console.log(data.results);
-    data.results.map( item => {
-        rootUl.innerHTML = rootUl.innerHTML + `
-        <li>${item.name.first} ${item.name.last}
-            <ul>
-            <li>${item.dob.age} anos</li>            
-            <li>${item.email}</li>
+    data.results.map((item) => {
+      rootUl.innerHTML =
+        rootUl.innerHTML +
+        `
+        <li class="card">
+            <div class="avatar"><img src='${item.picture.thumbnail}' /></div>
+            <ul class="card-text-info">
+                <li>${item.name.first} ${item.name.last}</li>
+                <li>${item.dob.age} anos</li>            
+                <li>${item.email}</li>
             </ul>
         </li>
         `;
     });
     spinner.style.opacity = 0;
-}).
-catch(error => { 
+  })
+  .catch((error) => {
     console.log(error);
     root.innerHTML = `Falha na comunicação: ${error}`;
-});
-
+  });
